8fc4695d18ff5b65c1186ef7e6316dde289525e4
Frédéric Parrenin <parrenif@xps13.home>
2023-05-05 11:11:41 +0200

    Cleaning of Clean.py and adding '*output.txt' to the files to be removed.

--- Clean.py
+++ Clean.py
@@ -1,10 +1,11 @@
-import os,re
+import os
+import re
 import sys
 
-dir=sys.argv[1]
-inclusive=True
+dir = sys.argv[1]
+inclusive = True
 
-pattern='.pdf'
+pattern = '.pdf'
 regexObj = re.compile(pattern)
 for root, dirs, files in os.walk(dir, topdown=False):
     for name in files:
@@ -16,4 +17,14 @@ for root, dirs, files in os.walk(dir, topdown=False):
         if len(os.listdir(path)) == 0:
             os.rmdir(path)
 
-
+pattern = 'output.txt'
+regexObj = re.compile(pattern)
+for root, dirs, files in os.walk(dir, topdown=False):
+    for name in files:
+        path = os.path.join(root, name)
+        if bool(regexObj.search(path)) == bool(inclusive):
+            os.remove(path)
+    for name in dirs:
+        path = os.path.join(root, name)
+        if len(os.listdir(path)) == 0:
+            os.rmdir(path)
